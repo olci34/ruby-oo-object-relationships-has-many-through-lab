a8d35a81a0e3f6ee1ddb5597933552d6112d07f6
github username <github email address>
2021-01-26 18:27:52 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,26 @@
 
+class Artist
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(song_name,genre)
+        song = Song.new(song_name, self, genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.collect {|song| song.genre}
+    end
+
+end
